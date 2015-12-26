2a19eac24b1b5acce232be055c3a85f1b77afb9e
guigur <guigur13@gmail.com>
2015-12-26 14:09:57 +0800

    fist test but the code doen't work now

--- 12.12PM/ex2/ex2.c
+++ 12.12PM/ex2/ex2.c
@@ -8,9 +8,52 @@
 #include <stropts.h>
 #include <sys/mman.h>
 
+#include <cpio.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <pthread.h>
+#include <pwd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <tar.h>
+#include <termios.h>
+#include <unistd.h>
+#include <utime.h>
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
 #define VIDEO_WIDTH  320
 #define VIDEO_HEIGHT 240
 #define VIDEO_DEPTH  16
+
+struct v4l2_format fmt; //Frame format, such
+int type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+fmt.fmt.pix.width = VIDEO_WIDTH;
+fmt.fmt.pix.height = VIDEO_HEIGHT;
+fmt.fmt.pix.depth = VIDEO_DEPTH;
+fmt.fmt.pix.pixelformat= V4L2_PIX_FMT_RGB565;
 
 int main()
 {
@@ -20,12 +63,14 @@ int main()
   int cambuf[size];
   char *fbmem_addr;
 
-  fdcam = open("/dev/video0", O_RDWR);
-  fdfbo = open("/dev/fbo", O_RDWR);
+  fdcam = open("/dev/urandom", O_RDWR);
+  fdfbo = open("/dev/fb0", O_RDWR);
   if (!fdcam || !fdfbo)
     printf("ERROR: Bad file descriptor\n");
   
   ioctl(fdfbo, FBIOGET_VSCREENINFO, &modeinfo);
+  ioctl(fdcam, VIDIOC_S_FMT, &fmt);
+  ioctl(fdcam, VIDIOC_STREAMON, &type);
   fbmem_addr = mmap(0, size, PROT_READ |
 		    PROT_WRITE, MAP_SHARED, fdfbo, 0);
   while (42)
